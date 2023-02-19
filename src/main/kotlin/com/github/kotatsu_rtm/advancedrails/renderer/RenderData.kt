ed22fb15f92d86b8a49cc6bc79ecd4175bc9942b
Siro_256 <siro@siro256.dev>
2023-02-19 16:49:57 +0900

    Cretate render data holder

---
+++ src/main/kotlin/com/github/kotatsu_rtm/advancedrails/renderer/RenderData.kt
@@ -0,0 +1,7 @@
+package com.github.kotatsu_rtm.advancedrails.renderer
+
+import com.github.kotatsu_rtm.advancedrails.model.ModelRegistry
+
+object RenderData {
+    val models = HashMap<String, ModelRegistry>()
+}
