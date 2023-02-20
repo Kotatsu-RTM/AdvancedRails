f3110f2e577f0146af95f33dd911ed8817ff9f6b
Siro_256 <siro@siro256.dev>
2023-02-20 23:43:55 +0900

    Fix typo

--- src/main/kotlin/com/github/kotatsu_rtm/advancedrails/renderer/RenderData.kt
+++ src/main/kotlin/com/github/kotatsu_rtm/advancedrails/renderer/RenderData.kt
@@ -6,10 +6,10 @@ object RenderData {
     val models = HashMap<String, ModelRegistry>()
 
     object OriginalScriptIdentifier {
-        val renderBERRails01js = "^.*(?:1067|1435|Morido)_(?:01)?[ABLR](?:_LH)?\$".toRegex()
-        val renderBERRails02js = "^.*(?:1067|1435)_02[AB][LR]\$".toRegex()
-        val renderBERRails03js = "^.*(?:1067|1435)_01C(?:_[LR])?\$".toRegex()
-        val renderBERRails04js = "^.*1435_CA\$".toRegex()
-        val renderBERRails05js = "^.*(?:1067|1435)_03G\$".toRegex()
+        val renderBRERails01js = "^.*(?:1067|1435|Morido)_(?:01)?[ABLR](?:_LH)?\$".toRegex()
+        val renderBRERails02js = "^.*(?:1067|1435)_02[AB][LR]\$".toRegex()
+        val renderBRERails03js = "^.*(?:1067|1435)_01C(?:_[LR])?\$".toRegex()
+        val renderBRERails04js = "^.*1435_CA\$".toRegex()
+        val renderBRERails05js = "^.*(?:1067|1435)_03G\$".toRegex()
     }
 }
