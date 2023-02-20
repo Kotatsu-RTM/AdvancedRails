c5b5c9510c2887d9eacb7e35a6bf2b2bf3509f7b
Siro_256 <siro@siro256.dev>
2023-02-21 01:53:49 +0900

    Create common renderer

---
+++ src/main/resources/assets/minecraft/scripts/Renderer.js
@@ -0,0 +1,6 @@
+var renderClass = "jp.ngt.rtm.render.RailPartsRenderer";
+importPackage(Packages.com.github.kotatsu_rtm.advancedrails.renderer)
+
+function init(modelSet, modelObject) {
+    RendererRouter.init(renderer, modelSet);
+}
