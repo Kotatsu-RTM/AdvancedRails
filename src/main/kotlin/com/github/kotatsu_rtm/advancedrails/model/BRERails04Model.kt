66995e93f806e68c891ef1237ecad8fc893f5be0
Siro_256 <siro@siro256.dev>
2023-02-20 23:27:04 +0900

    Merge BRERails model files

--- src/main/kotlin/com/github/kotatsu_rtm/advancedrails/model/BRERails04Model.kt
+++
@@ -1,8 +0,0 @@
-package com.github.kotatsu_rtm.advancedrails.model
-
-import jp.ngt.rtm.render.PartsRenderer
-
-class BRERails04Model(renderer: PartsRenderer<*, *>): ModelRegistry(renderer) {
-    val anchorL = registerParts("anchorL")
-    val anchorR = registerParts("anchorR")
-}
