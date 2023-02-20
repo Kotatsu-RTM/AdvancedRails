079cf8750e3f3333d461684c58b48cdcf24b2341
Siro_256 <siro@siro256.dev>
2023-02-20 22:59:05 +0900

    Bugfix
    Why you don't use argument 'x'?????????

--- src/main/kotlin/com/github/kotatsu_rtm/advancedrails/Util.kt
+++ src/main/kotlin/com/github/kotatsu_rtm/advancedrails/Util.kt
@@ -7,7 +7,7 @@ fun Float.toRadians() = this * (PI / 180).toFloat()
 
 object Util {
     fun sigmoid(x: Float): Float {
-        val a = 3.5F
+        val a = x * 3.5F
         return a / MathHelper.sqrt(1.0F + a * a) * 0.75F + 0.25F
     }
 }
