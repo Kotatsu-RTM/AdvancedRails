2cf262b9a0686ccaf9159eb316479ddeee2e94b6
Siro_256 <siro@siro256.dev>
2023-02-20 18:18:22 +0900

    Make original script identification regex to variable

--- src/main/kotlin/com/github/kotatsu_rtm/advancedrails/renderer/RenderData.kt
+++ src/main/kotlin/com/github/kotatsu_rtm/advancedrails/renderer/RenderData.kt
@@ -4,4 +4,12 @@ import com.github.kotatsu_rtm.advancedrails.model.ModelRegistry
 
 object RenderData {
     val models = HashMap<String, ModelRegistry>()
+
+    object OriginalScriptIdentifier {
+        val renderBERRails01js = "^.*(?:1067|1435|Morido)_(?:01)?[ABLR](?:_LH)?\$".toRegex()
+        val renderBERRails02js = "^.*(?:1067|1435)_02[AB][LR]\$".toRegex()
+        val renderBERRails03js = "^.*(?:1067|1435)_01C(?:_[LR])?\$".toRegex()
+        val renderBERRails04js = "^.*1435_CA\$".toRegex()
+        val renderBERRails05js = "^.*(?:1067|1435)_03G\$".toRegex()
+    }
 }
