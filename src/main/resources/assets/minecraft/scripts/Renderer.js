c2b3f406d9536704c411ac4b7e315b681992433a
Siro_256 <siro@siro256.dev>
2023-02-21 02:06:07 +0900

    Fix wrong argument

--- src/main/resources/assets/minecraft/scripts/Renderer.js
+++ src/main/resources/assets/minecraft/scripts/Renderer.js
@@ -2,5 +2,5 @@ var renderClass = "jp.ngt.rtm.render.RailPartsRenderer";
 importPackage(Packages.com.github.kotatsu_rtm.advancedrails.router)
 
 function init(modelSet, modelObject) {
-    RendererRouter.init(renderer, modelSet);
+    RendererRouter.init(renderer, modelObject);
 }
